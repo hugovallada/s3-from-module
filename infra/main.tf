2be86c0078fbcb1a80a591092e1177036245b1fb
Hugo Vallada <valladahugo@gmail.com>
2022-10-10 06:58:56 -0300

    Modifica variavel

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "bucket" {
   source            = "git::https://github.com/hugovallada/s3-module"
-  bucket_name       = "s3-module"
+  bucket_name       = "new-glacier-bucket"
   public_access     = true
-  lyfecicle_enabled = true
+  lifecycle_enabled = true
   days              = 20
 }
