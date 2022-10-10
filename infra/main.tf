e041ff1e81ac8cff7a8dbdc5ff0ae0d8f4519dac
Hugo Vallada <valladahugo@gmail.com>
2022-10-10 06:19:23 -0300

    Ajustes

--- infra/main.tf
+++ infra/main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.31.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "hugovallada-tfstate"
-    key    = "tf-state/[nome_do_projeto]/terraform.tfstate" //TODO: Substituir pelo nome do projeto
-    region = "sa-east-1"
-  }
-}
-
-provider "aws" {
-  region = "sa-east-1"
-
-  default_tags {
-    tags = {
-      owner      = "hugovallada"
-      managed-by = "terraform"
-    }
-  }
-}
-
 module "bucket" {
   source            = "github.com/hugovallada/s3-from-module"
   bucket_name       = "s3-module"
